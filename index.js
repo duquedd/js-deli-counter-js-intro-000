d6fa8d409d24a2593e987623b6c6f455aaa610b0
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-18 01:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,14 +12,17 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  var retVar = "The line is currently:"
+  var retVar = ""
 
   for(var quantity=katzDeliLine.length;quantity>0;quantity){
     if (!(quantity=== katzDeliLine.length)){ retVar = `,`+retVar}
     retVar=` ${quantity}. ${katzDeliLine[quantity-1]}`+ retVar
     quantity--
   }
-return retVar
+  if (!(retVar===""){
+    return `The line is currently:`+ retVar
+  }
+
 
 
 }
