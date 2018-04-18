0e8ffb53c9814f16203cab450fb23e0719ccfaaa
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-18 00:20:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber(katzDeliLine,newPersonInLine){
+  katzDeliLine.push(newPersonInLine)
+  return `Welcome, ${newPersonInLine}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  
+}
+
+function currentLine(katzDeliLine){
+  
+}
