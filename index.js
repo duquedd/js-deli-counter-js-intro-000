ecb1808e179bfad3188825af88ceb70f424ff554
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-18 00:31:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function takeANumber(katzDeliLine,newPersonInLine){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length>0) {
-    return `Currently serving ${katzDeliLine[0]}.`
-    katzDeliLine.unshift()
+    return `Currently serving ${katzDeliLine.shift()}.`
   }else {
     return `There is nobody waiting to be served!`
   }      //Verified length function can never be negative
